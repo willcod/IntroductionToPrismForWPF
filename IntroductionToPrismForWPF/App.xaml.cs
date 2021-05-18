9f61b08a533e94122c79d412f2d4df5a4ba26aca
willcod <william.wow@icloud.com>
2021-05-18 11:43:29 +0800

    ceate a shell and custom RegionAdapter for StackPanel

--- IntroductionToPrismForWPF/App.xaml.cs
+++ IntroductionToPrismForWPF/App.xaml.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IntroductionToPrismForWPF.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
 using System.Windows;
+using System.Windows.Controls;
+using IntroductionToPrismForWPF.Core;
+using Prism.Regions;
 
 namespace IntroductionToPrismForWPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+        }
     }
 }
---
+++ IntroductionToPrismForWPF/Core/StackPanelRegionAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Prism.Regions;
+
+namespace IntroductionToPrismForWPF.Core
+{
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(regionBehaviorFactory)
+        {
+        }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach (FrameworkElement item in e.NewItems)
+                    {
+                        regionTarget.Children.Add(item);
+                    }
+                } else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (FrameworkElement item in e.OldItems)
+                    {
+                        regionTarget.Children.Remove(item);
+                    }
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
