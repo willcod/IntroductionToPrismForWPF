34a2188da08601a05b5baa848cc7538d3499d677
willcod <william.wow@icloud.com>
2021-05-19 13:56:13 +0800

    View injection

--- MainModule/MainModule.cs
+++ MainModule/MainModule.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using MainModule.Views;
 using Prism.Ioc;
 using Prism.Modularity;
@@ -23,7 +25,22 @@ namespace MainModule
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
-            _regionManager.RegisterViewWithRegion("MainRegion", typeof(ViewA));
+            //_regionManager.RegisterViewWithRegion("MainRegion", typeof(ViewA));
+            var region = _regionManager.Regions["MainRegion"];
+            var view = containerProvider.Resolve<ViewA>();
+            region.Add(view);
+
+            var anotherView = containerProvider.Resolve<ViewA>();
+            anotherView.Content = new TextBlock()
+            {
+                Text = "Hello from another View",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                FontSize = 48
+            };
+
+            region.Add(anotherView);
+            region.Remove(view);
         }
     }
 }
