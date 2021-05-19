061769e2744cef4d9f877468094afbbc24ee62db
willcod <william.wow@icloud.com>
2021-05-19 13:22:28 +0800

    View and View Discovery

--- MainModule/MainModule.cs
+++ MainModule/MainModule.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MainModule.Views;
 using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 
 namespace MainModule
 {
     public class MainModule : IModule
     {
+        private IRegionManager _regionManager;
+        public MainModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
         }
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(ViewA));
         }
     }
 }
