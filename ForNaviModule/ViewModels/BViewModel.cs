7d3e696ef3fc1089acfe9bf267880b4d9c571518
willcod <william.wow@icloud.com>
2021-05-20 15:32:31 +0800

    ViewModel participation

---
+++ ForNaviModule/ViewModels/BViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace ForNaviModule.ViewModels
+{
+    public class BViewModel:BindableBase, INavigationAware
+    {
+        private int _viewCount = 0;
+
+        public int ViewCount {
+            get {
+                return _viewCount;
+            }
+            set {
+                SetProperty(ref _viewCount, value);
+            }
+        }
+
+        public BViewModel() {
+            ViewCount = 0;
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext) {
+            ViewCount++;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext) {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext) {
+        }
+    }
+}
